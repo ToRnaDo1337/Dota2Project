a3b224befc4bf18ab13d6da79f59dceacd9ce944
Eldar <moon_999@mail.ru>
2022-11-02 15:17:40 +0600

    Added navigation Drawer, but with error

--- app/src/main/java/com/example/dota2/MainActivity.kt
+++ app/src/main/java/com/example/dota2/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.dota2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.MenuItem
+
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+
+
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        // drawer layout instance to toggle the menu icon to open
+//        // drawer and back button to close drawer
+//        drawerLayout = findViewById(R.id.activity_main)
+//
+//        // pass the Open and Close toggle for the drawer layout listener
+//        // to toggle the button
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+//        actionBarDrawerToggle.syncState()
+//
+//        // to make the Navigation drawer icon always appear on the action bar
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
