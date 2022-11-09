c105fb52d824af1f1cff8bd4c86ce8980b9ca1ce
Magzhan <200103369@stu.sdu.edu.kz>
2022-11-09 14:00:24 +0600

    added logout system in else statement on LoginRepository.kt

--- app/src/main/java/com/example/dota2/ui/aboutdota/AboutFragment.kt
+++ app/src/main/java/com/example/dota2/ui/aboutdota/AboutFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.dota2.R
 
 import com.example.dota2.databinding.FragmentAboutBinding
 
